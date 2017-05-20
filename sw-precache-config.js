1cdaa79a9e089194644b1c32d0006f194db04ba9
Elliott Marquez <ElliottAMarquez@gmail.com>
2017-05-20 00:52:58 -0700

    add sw

---
+++ sw-precache-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  staticFileGlobs: [
+    '/index.html',
+    '/manifest.json',
+    '/bower_components/webcomponentsjs/webcomponents-lite.js'
+  ],
+  navigateFallback: '/index.html'
+};
