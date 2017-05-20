a9a4d3baf6db7e3e59303aeb5b6ce38afee2f57d
Elliott Marquez <ElliottAMarquez@gmail.com>
2017-05-20 01:05:22 -0700

    strategic caching

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -1,8 +1,41 @@
+const path = 'https://localhost:5000';
+const version = 0;
+
 module.exports = {
   staticFileGlobs: [
     '/index.html',
     '/manifest.json',
     '/bower_components/webcomponentsjs/webcomponents-lite.js'
   ],
-  navigateFallback: '/index.html'
+  runtimeCaching: [
+    {
+      urlPattern: new RegExp(`${path}/r/.+/$`),
+      handler: 'fastest',
+      options: {
+        cache: {
+          maxEntries: 100,
+          name: `posts-cache-${version}`
+        }
+      }
+    }, {
+      urlPattern: new RegExp(`${path}/r/.*/comments/.+`),
+      handler: 'fastest',
+      options: {
+        cache: {
+          maxEntries: 30,
+          name: `comments-cache-${version}`
+        }
+      }
+    }, {
+      urlPattern: new RegExp(`${path}/u/.+`),
+      handler: 'fastest',
+      options: {
+        cache: {
+          maxEntries: 30,
+          name: `user-cache-${version}`
+        }
+      }
+    }
+  ],
+  navigateFallback: 'index.html',
 };
